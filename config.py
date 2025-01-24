742c7baa9030338f607e85a319216011dd4e82bb
inoplatform <inoksan.platform@gmail.com>
2025-01-24 16:52:02 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LANGUAGES = ['en', 'ar', 'tr', 'sp', 'ru']
